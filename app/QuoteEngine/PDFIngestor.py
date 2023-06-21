78175de24295f9c7ddc6b104c1f12d232686249d
Christoph Eck <christoph@milkinteractive.ch>
2023-06-21 08:21:29 +0200

    fix: pdfingestor - return an empty model if tool is not available

--- app/QuoteEngine/PDFIngestor.py
+++ app/QuoteEngine/PDFIngestor.py
@@ -1,5 +1,6 @@
 """PDFIngestor supports pdf files to be parsed and extracts QuoteModels from.
 
+You need to have the tool pdftotext installed otherwise it returns an empty array.
 The pdf formatting lines are defined by "quote - author".
 """
 
@@ -38,12 +39,11 @@ class PDFIngestor(IngestorInterface):
         """
         cls.can_ingest_or_throw(path)
         cls.check_path_exists_or_throw(path)
-        if not cls.__is_tool_available("pdftotext"):
-            raise Exception(
-                "The tool pdftotext is not available. Please first install the tool."
-            )
-
         models = []
+
+        if not cls.__is_tool_available("pdftotext"):
+            return models
+
         tmp: str = None
         try:
             if not os.path.exists("./tmp"):
